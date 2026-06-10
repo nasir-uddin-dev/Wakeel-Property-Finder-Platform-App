d7b5e7d4e0b83fcf8f47040080feef843256ea36
PC <nasiruddin3954@gmail.com>
2026-06-10 06:27:41 +0600

    Add signup screen enhancements and new dependencies
    - Updated the signup screen to include fields for National ID, License Number, and a phone number input.
    - Added new dependencies: intl_phone_number_input and dotted_decoration.
    - Improved layout with SingleChildScrollView for better usability.
    - Added an icon for National ID input.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-   static const Color primaryColor = Color(0xFF007267);
+  static const Color primaryColor = Color(0xFF007267);
   static const Color textLabelColor = Color(0xFF414141);
   static const Color hintTextColor = Color(0xFF6B6B6B);
   static const Color inputBorderColor = Color(0xFFECECEC);
+  static const Color containerColor = Color(0xFFE8FFFD);
+  
 }
--- lib/features/signup/signup_screen.dart
+++ lib/features/signup/signup_screen.dart
@@ -1,5 +1,7 @@
+import 'package:dotted_decoration/dotted_decoration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:wakeel_property_finder_platform_app/core/constants/app_colors.dart';
 import 'package:wakeel_property_finder_platform_app/core/shared/common_widgets/custom_button.dart';
 import 'package:wakeel_property_finder_platform_app/core/shared/common_widgets/custom_text_form_field.dart';
@@ -14,15 +16,23 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final _fullNameController = TextEditingController();
   final _userNameController = TextEditingController();
+  final _nationalIdController = TextEditingController();
   final _passwordController = TextEditingController();
-
+  final _reTypePasswordController = TextEditingController();
+  final _licenseNumberController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
+  PhoneNumber? phoneNumber;
+  bool isValidNumber = false;
 
   @override
   void dispose() {
     _fullNameController.dispose();
     _userNameController.dispose();
+    _nationalIdController.dispose();
     _passwordController.dispose();
+    _reTypePasswordController.dispose();
+    _licenseNumberController.dispose();
     super.dispose();
   }
 
@@ -32,140 +42,284 @@ class _SignUpScreenState extends State<SignUpScreen> {
       backgroundColor: Color(0xFFFFFFFF),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            56.verticalSpace,
-            Center(
-              child: Image.asset(
-                'assets/images/login_logo.png',
-                width: 61.w,
-                height: 40.h,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              56.verticalSpace,
+              Center(
+                child: Image.asset(
+                  'assets/images/login_logo.png',
+                  width: 61.w,
+                  height: 40.h,
+                ),
               ),
-            ),
+              40.verticalSpace,
+              Text(
+                "Create Account",
+                style: TextStyle(
+                  color: Color(0xFF000000),
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              24.verticalSpace,
+              Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Full Name*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _fullNameController,
+                      hintText: 'Enter a full name',
+                      textInputAction: TextInputAction.next,
+                      prefixIcon: Image.asset(
+                        'assets/icons/user_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'User Name*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _userNameController,
+                      hintText: 'Enter a user name',
+                      textInputAction: TextInputAction.next,
+                      prefixIcon: Image.asset(
+                        'assets/icons/user_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'Phone Number*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    InternationalPhoneNumberInput(
+                      onInputChanged: (PhoneNumber number) {
+                        phoneNumber = number;
+                      },
+                      onInputValidated: (bool value) {
+                        setState(() {
+                          isValidNumber = value;
+                        });
+                      },
 
-            40.verticalSpace,
-            Text(
-              "Create Account",
-              style: TextStyle(
-                color: Color(0xFF000000),
-                fontSize: 18.sp,
-                fontWeight: FontWeight.w700,
+                      autoValidateMode: AutovalidateMode.onUserInteraction,
+                      errorMessage: 'Invalid phone number',
+                      selectorConfig: SelectorConfig(
+                        selectorType: PhoneInputSelectorType.DROPDOWN,
+                        setSelectorButtonAsPrefixIcon: true,
+                        leadingPadding: 20,
+                        trailingSpace: false,
+                      ),
+                      inputDecoration: InputDecoration(
+                        hintText: 'Enter your phone number',
+                        hintStyle: TextStyle(
+                          color: AppColors.hintTextColor,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                          borderSide: BorderSide(
+                            color: AppColors.inputBorderColor,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                          borderSide: BorderSide(
+                            color: AppColors.inputBorderColor,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                          borderSide: BorderSide(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+
+                    16.verticalSpace,
+                    Text(
+                      'National ID*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _nationalIdController,
+                      hintText: 'Enter a national id',
+                      textInputAction: TextInputAction.next,
+
+                      prefixIcon: Image.asset(
+                        'assets/icons/id_card_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'Password*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _passwordController,
+                      hintText: 'Enter a password',
+                      textInputAction: TextInputAction.next,
+                      suffixIcon: Icon(
+                        Icons.visibility_off,
+                        color: AppColors.inputBorderColor,
+                        size: 24.sp,
+                      ),
+                      obscureText: true,
+                      prefixIcon: Image.asset(
+                        'assets/icons/lock_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'Re-type Password*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _reTypePasswordController,
+                      hintText: 'Enter a re-type password',
+                      textInputAction: TextInputAction.next,
+                      suffixIcon: Icon(
+                        Icons.visibility_off,
+                        color: AppColors.inputBorderColor,
+                        size: 24.sp,
+                      ),
+                      obscureText: true,
+                      prefixIcon: Image.asset(
+                        'assets/icons/lock_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'License Number*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    CustomTextFormField(
+                      controller: _licenseNumberController,
+                      hintText: 'Enter a license number',
+                      textInputAction: TextInputAction.done,
+
+                      prefixIcon: Image.asset(
+                        'assets/icons/id_card_icon.png',
+                        width: 10.w,
+                        height: 10.h,
+                      ),
+                    ),
+                    16.verticalSpace,
+                    Text(
+                      'License Certification File*',
+                      style: TextStyle(
+                        color: AppColors.textLabelColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    8.verticalSpace,
+                    Container(
+                      width: double.infinity,
+                      height: 56.h,
+                      decoration: DottedDecoration(
+                        shape: Shape.box,
+                        color: AppColors.primaryColor,
+                        dash: [4,4],
+                        strokeWidth: 1.5,
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
+                    ),
+                    24.verticalSpace,
+                    CustomButton(
+                      onTap: () {},
+                      text: 'Sign Up',
+                      textColor: Color(0xFFFFFFFF),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            24.verticalSpace,
-            Form(
-              key: _formKey,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              24.verticalSpace,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Full Name*',
+                    "Don't have an account yet?",
                     style: TextStyle(
-                      color: AppColors.textLabelColor,
+                      color: Colors.black,
                       fontSize: 14.sp,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                  8.verticalSpace,
-                  CustomTextFormField(
-                    controller: _fullNameController,
-                    hintText: 'Enter a full name',
-                    textInputAction: TextInputAction.next,
-                    prefixIcon: Image.asset(
-                      'assets/icons/user_icon.png',
-                      width: 10.w,
-                      height: 10.h,
+                  4.horizontalSpace,
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                  16.verticalSpace,
-                  Text(
-                    'User Name*',
-                    style: TextStyle(
-                      color: AppColors.textLabelColor,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  8.verticalSpace,
-                  CustomTextFormField(
-                    controller: _userNameController,
-                    hintText: 'Enter a user name',
-                    textInputAction: TextInputAction.next,
-                    prefixIcon: Image.asset(
-                      'assets/icons/user_icon.png',
-                      width: 10.w,
-                      height: 10.h,
-                    ),
-                  ),
-                  16.verticalSpace,
-                  Text(
-                    'Password',
-                    style: TextStyle(
-                      color: AppColors.textLabelColor,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  8.verticalSpace,
-                  CustomTextFormField(
-                    controller: _passwordController,
-                    hintText: 'Enter a password',
-                    textInputAction: TextInputAction.done,
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye_outlined,
-                      color: AppColors.inputBorderColor,
-                      size: 24.sp,
-                    ),
-                    obscureText: true,
-                    prefixIcon: Image.asset(
-                      'assets/icons/lock_icon.png',
-                      width: 10.w,
-                      height: 10.h,
-                    ),
-                  ),
-
-                  24.verticalSpace,
-                  CustomButton(
-                    onTap: () {},
-                    text: 'Sign Up',
-                    textColor: Color(0xFFFFFFFF),
                   ),
                 ],
               ),
-            ),
-            24.verticalSpace,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account yet?",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                4.horizontalSpace,
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      color: AppColors.primaryColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Spacer(),
-            Image.asset('assets/images/rectangle.png'),
-          ],
+              24.verticalSpace,
+              Image.asset('assets/images/rectangle.png'),
+            ],
+          ),
         ),
       ),
     );
